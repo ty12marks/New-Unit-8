3dfaa844720776d698e8f4be7232174b99898ce8
ty12marks <ty12marks@gmail.com>
2024-04-08 11:36:48 -0400

    Created files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
+const supabaseUrl = 'https://unppumaqumdqwvomiqpk.supabase.co'
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucHB1bWFxdW1kcXd2b21pcXBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MDAwMDAsImV4cCI6MjAyNjM3NjAwMH0.kx3X-HJI41c-63A7vPFUVrLIYJVxsbjAAojhAKLThGQ'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks() {
+let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+for (let book of books) {
+  let booklist = document.getElementById('books');
+  booklist.innerHTML += `<li>${book.title} - ${book.author}</li>`;
+}
+}
+
+getBooks();
